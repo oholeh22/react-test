922b5110d2f13740050c53f09e8e7c9ab255cf93
Oleh Shyian <shiyan4242@gmail.com>
2024-10-18 15:55:43 +0200

    fix react

---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+// src/Product.jsx
+export default function Product() {
+        return (
+          <div>
+            <h2>Cookies</h2>
+          <p>Price: 999 credits</p>
+          </div>
+        );
+    };
+    
